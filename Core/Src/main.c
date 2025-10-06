0d32509d4fc9d682ca44a0a2b717f9b0706b977e
Nikolai Kozlov <ru.dev47@trezub.ru>
2025-10-06 19:14:50 +0300

    corner rectangles added

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -15,8 +15,58 @@ int main(void)
     printf("START.\r\n");
     printf("SYSCLK = %d\r\n", (int)SYSCLK);
 
+    PINB_2_INIT();
+
     while (1)
     {
+        ST7789_Fill_Color(BLACK);
+        delay(3000);
+
+        GPIOB->BSRR = GPIO_BSRR_BS2;
+        ST7789_Fill_Color(RED);
+        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR2;
+        delay(3000);
+
+        GPIOB->BSRR = GPIO_BSRR_BS2;
+        ST7789_WriteString(10, 10, "Rect./Line.", Font_11x18, YELLOW, BLACK);
+        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR2;
+        delay(3000);
+
+        GPIOB->BSRR = GPIO_BSRR_BS2;
+        ST7789_DrawRectangle(30, 30, 100, 100, WHITE);
+        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR2;
+        delay(3000);
+        //--------------------------------
+        GPIOB->BSRR = GPIO_BSRR_BS2;
+        ST7789_DrawRectangle(0, 0, 50, 50, GREEN);
+        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR2;
+        delay(3000);
+
+        GPIOB->BSRR = GPIO_BSRR_BS2;
+        ST7789_DrawRectangle(0, 230, 50, 279, GREEN);
+        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR2;
+        delay(3000);
+
+        GPIOB->BSRR = GPIO_BSRR_BS2;
+        ST7789_DrawRectangle(190, 0, 239, 50, GREEN);
+        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR2;
+        delay(3000);
+
+        GPIOB->BSRR = GPIO_BSRR_BS2;
+        ST7789_DrawRectangle(190, 230, 239, 279, GREEN);
+        delay(500);
+        GPIOB->BSRR = GPIO_BSRR_BR2;
+        delay(3000);
+        //----------------------------------
+        printf("ST7789_DrawRectangle(30, 30, 100, 100, WHITE);\r\n");
+        delay(10000);
+
         ST7789_Test();
         delay(500);
     }
